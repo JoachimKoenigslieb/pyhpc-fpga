00e7c49ddba593dde7002e06b4a27f1ede565169
Joachim Kønigslieb <joachim.koenigslieb@gmail.com>
2020-10-26 11:49:33 +0100

    this works! we can broadcast. just transpile a lot of numpy code now

--- src/host.cpp
+++ src/host.cpp
@@ -34,38 +34,48 @@ void print_vec(std::vector<int> v){
 }
 
 
-std::vector<int> calculate_shapes_for_broadcasting(std::vector<int> &shape_A, std::vector<int> &shape_B){
-	int len_A = shape_A.size(), len_B = shape_B.size();
-
-	std::cout << "A has len " << len_A << " B has len: " << len_B << std::endl;
-
-	if (len_A < len_B){
-		for (int i=0; i<(len_B-len_A); i++){
-			shape_A.insert(shape_A.begin(), 1); // O(n) but we dont care. Insert singeleton dimensions
-			std::cout << "insert singleton at A" << std::endl;
-		}
-	} else {
-		for (int i=0; i<(len_A - len_B); i++){
-			shape_B.insert(shape_B.begin(), 1);
-			std::cout << "insert singleton at B" << std::endl;
-		}
+void pad_begin(std::vector<int> &vec, int val, int to_len){
+	int vec_len = vec.size();
+	for (int i=0; i<(to_len - vec_len); i++){
+		vec.insert(vec.begin(), val);
 	}
+}
+
+void calculate_shapes_for_broadcasting(std::vector<int> &shape_A, std::vector<int> &shape_B, std::vector<int> requested_shape_output)
+{
+	int len_O = requested_shape_output.size();
+	int len_A = shape_A.size();
+	int len_B = shape_B.size();
+
+	std::cout << "A has len " << len_A << " B has len: " << len_B << " requested output has len " << len_O << std::endl;
+
+	pad_begin(shape_A, 1, len_O);
+	pad_begin(shape_B, 1, len_O);
 
 	// loop trough and check if we can broadcast
-	int dim_A, dim_B;
-	std::vector<int> shape_output(std::max(len_A, len_B)); 
+	int dim_A, dim_B, dim_O, naive_output;
 
-	for (int i=0; i<std::max(len_A, len_B); i++){
+	for (int i = 0; i < len_O; i++)
+	{
 		dim_A = shape_A[i];
 		dim_B = shape_B[i];
-		if (!(dim_A == dim_B || dim_A == 1 || dim_B == 1)){
-			std::cout << "You tried to broadcast shapes that does not broadcast\n"; //do proper error handling
-		}
-		shape_output[i] = std::max(dim_A, dim_B);
-	}
+		dim_O = requested_shape_output[i];
 
-	return shape_output;
+		if (!(dim_A == dim_B || dim_A == 1 || dim_B == 1))
+		{
+			std::cout << "!!!\n!!!\nERROR: You tried to broadcast shapes that does not broadcast\n!!!\n!!!\n!!!\n"; //do proper error handling
+		}
+
+		naive_output = std::max(dim_A, dim_B); //this is max of inputs shapes. should be either 1, or equal to requested shape!
+		std::cout << "broadcasting the inputs gives: " << naive_output << ". We are requesting: " << dim_O << std::endl;
+
+		if (!(dim_O == naive_output || naive_output == 1))
+		{
+			std::cout << "!!!\n!!!\nERROR: You tried to broadcast into a shape that can not be broadcast from the inputs!\n!!!\n!!!\n!!!\n";
+		}
+	}
 }
+
 
 std::vector<int> get_stride_from_shape(std::vector<int> shape){
 	int dims = shape.size();
@@ -100,106 +110,217 @@ unsigned long diff(const struct timeval *newTime, const struct timeval *oldTime)
 	return (newTime->tv_sec - oldTime->tv_sec) * 1000000 + (newTime->tv_usec - oldTime->tv_usec);
 }
 
-void run_kernel(std::string kernel_name, std::vector<double *> &inputs, std::vector<double *> &outputs, std::vector<int> A_shape, std::vector<int> B_shape, std::vector<cl::Device> &devices, cl::Context &context, cl::Program::Binaries &bins, cl::CommandQueue &q)
+void run_kernel(std::string kernel_name, int kernel_size, std::vector<double *> &inputs, std::vector<double *> &outputs, std::vector<cl::Device> &devices, cl::Context &context, cl::Program::Binaries &bins, cl::CommandQueue &q)
+{
+        // this is a helper function to execute a kernel.
+        {
+                cl::Program program(context, devices, bins); //Note. we use devices not device here!!!
+                cl::Kernel kernel(program, kernel_name.data());
+
+                std::cout << "INFO: Kernel '" << kernel_name << "' has been created" << std::endl;
+
+                int num_in = inputs.size();
+                int num_out = outputs.size();
+
+                std::vector<cl::Buffer> in_buffers(num_in);
+                std::vector<cl::Buffer> out_buffers(num_out);
+
+                for (int i = 0; i < num_in; i++)
+                {
+                        in_buffers[i] = cl::Buffer(context, CL_MEM_USE_HOST_PTR | CL_MEM_READ_ONLY, sizeof(double) * kernel_size, inputs[i]);
+                }
+
+                for (int i = 0; i < num_out; i++)
+                {
+                        out_buffers[i] = cl::Buffer(context, CL_MEM_USE_HOST_PTR | CL_MEM_WRITE_ONLY, sizeof(double) * kernel_size, outputs[i]);
+                }
+
+                std::cout << "INFO: Buffers has been created" << std::endl;
+
+                kernel.setArg(0, kernel_size); //we need to set arg0 to size!
+
+                for (int i = 0; i < num_in; i++)
+                {
+                        kernel.setArg(i + 1, in_buffers[i]);
+                        q.enqueueMigrateMemObjects({in_buffers[i]}, 0);
+                }
+
+                for (int i = 0; i < num_out; i++)
+                {
+                        kernel.setArg(i + num_in + 1, out_buffers[i]);
+                }
+
+                std::cout << "INFO: Arguments has been set" << std::endl;
+
+                std::cout << "INFO: Migrated to device" << std::endl;
+
+                q.enqueueTask(kernel);
+
+                q.finish();
+
+                q.enqueueMigrateMemObjects({out_buffers[0]}, CL_MIGRATE_MEM_OBJECT_HOST); // 1 : migrate from dev to host
+
+                std::cout << "INFO: Migrated to back to host" << std::endl;
+
+                q.finish();
+        }
+}
+
+
+void run_broadcast_kernel(std::string kernel_name,
+							std::vector<double *> &inputs,
+							std::vector<double *> &outputs,
+							std::vector<int> A_shape,
+							std::vector<int> B_shape,
+							std::vector<int> output_shape,
+							std::vector<int> A_offset,
+							std::vector<int> B_offset,
+							std::vector<int> output_offset,
+							std::vector<int> A_offset_end,
+							std::vector<int> B_offset_end,
+							std::vector<int> output_offset_end,
+							std::vector<cl::Device> &devices,
+							cl::Context &context,
+							cl::Program::Binaries &bins,
+							cl::CommandQueue &q)
 {
 	// this is a helper function to execute a kernel.
 	{
+		//setup program and kernel:
 		cl::Program program(context, devices, bins); //Note. we use devices not device here!!!
 		cl::Kernel kernel(program, kernel_name.data());
-
 		std::cout << "INFO: Kernel '" << kernel_name << "' has been created" << std::endl;
 
+		//num inputs outputs
 		int num_in = inputs.size();
 		int num_out = outputs.size();
 
+		//setup buffers:
 		std::vector<cl::Buffer> in_buffers(num_in);
-		std::vector<cl::Buffer> out_buffers(num_out);
 		std::vector<cl::Buffer> in_stride_buffers(num_in);
-		std::vector<cl::Buffer> out_stride_buffers(num_out);
+		std::vector<cl::Buffer> in_offset_buffers(num_in);
+
+		cl::Buffer out_buffer;
+		cl::Buffer out_shape_buffer;
+		cl::Buffer out_stride_buffer;
+		cl::Buffer out_offset_buffer;
+		cl::Buffer out_offset_end_buffer;
+
+		//calculate size variables
+		int dimensions = output_shape.size();
+		pad_begin(A_shape, 1, dimensions); //add singleton dimensions
+		pad_begin(B_shape, 1, dimensions);
+		pad_begin(A_offset, 0, dimensions); //zero pad offsets if not defined
+		pad_begin(B_offset, 0, dimensions);
+		pad_begin(A_offset_end, 0, dimensions);
+		pad_begin(B_offset_end, 0, dimensions);
+		for (int i=0; i<dimensions; i++){
+			A_offset[i] -= output_offset[i];
+			B_offset[i] -= output_offset[i];
+		}
+
+		std::vector<int> data_sizes_input = {cumprod(A_shape), cumprod(B_shape)};
+		int data_size_output = cumprod(output_shape);
 
 
-		std::vector<int> output_shape = calculate_shapes_for_broadcasting(A_shape, B_shape);  //here we modify A_shape and B_shape inplace. we have a copy of them in the run_kernel scope.
-		int len_stride = A_shape.size();
+		//calculate strides and offsets
+		std::vector<std::vector<int>> input_strides = {get_stride_from_shape(A_shape), get_stride_from_shape(B_shape)};
+		std::vector<int> output_stride = get_stride_from_shape(output_shape);
+		std::vector<std::vector<int>> input_offset = {A_offset, B_offset};
 
-		cl::Buffer out_shape_buffer = cl::Buffer(context, CL_MEM_USE_HOST_PTR | CL_MEM_WRITE_ONLY, sizeof(int) * output_shape.size(), output_shape.data());
-
+		//print debug info:
 		std::cout << "A_shapes: ";
 		print_vec(A_shape);
-		std::cout << "B_shapes: "; 
+		std::cout << "B_shapes: ";
 		print_vec(B_shape);
 		std::cout << "Out_shapes ";
 		print_vec(output_shape);
 
-		std::vector<std::vector<int>> input_strides = {get_stride_from_shape(A_shape),get_stride_from_shape(B_shape)};
-		std::vector<std::vector<int>> output_strides = {get_stride_from_shape(output_shape)};
+		std::cout << "A_offset: ";
+		print_vec(A_offset);
+		std::cout << "B_offset: ";
+		print_vec(B_offset);
+		std::cout << "out_offset: ";
+		print_vec(output_offset);
 
-		/* manual override for emergency situations....
 
-		std::vector<int> A_stride = {0, 1};
-		std::vector<int> B_stride = {10, 1};
-		std::vector<int> O_stride = {10, 1};
-
-		input_strides = {A_stride, B_stride};
-		output_strides = {O_stride};
-
-		*/
+		std::cout << "A_offset_end: ";
+		print_vec(A_offset_end);		
+		std::cout << "B_offset_end: ";
+		print_vec(B_offset_end);		
+		std::cout << "out_offset_end: ";
+		print_vec(output_offset_end);
 
 		std::cout << "A strides: ";
 		print_vec(input_strides[0]);
 		std::cout << "B strides: ";
 		print_vec(input_strides[1]);
 		std::cout << "O strides: ";
-		print_vec(output_strides[0]);
+		print_vec(output_stride);
 
-		std::vector<int> input_sizes = {cumprod(A_shape), cumprod(B_shape)};
-		std::vector<int> output_sizes = {cumprod(output_shape)};
+		std::cout << "Input size: " << data_sizes_input[0] << ", " << data_sizes_input[1] << std::endl;
+		std::cout << "Output size: " << data_size_output << std::endl;
 
-		std::cout << "Input size: " << input_sizes[0] << ", " << input_sizes[1] << std::endl;
-		std::cout << "Output size: " << output_sizes[0] << std::endl; 
-
+		// write to buffers
 		for (int i = 0; i < num_in; i++){
-			in_buffers[i] = cl::Buffer(context, CL_MEM_USE_HOST_PTR | CL_MEM_READ_ONLY, sizeof(double) * input_sizes[i], inputs[i]);
-			in_stride_buffers[i] = cl::Buffer(context, CL_MEM_USE_HOST_PTR | CL_MEM_READ_ONLY, sizeof(int) * len_stride, input_strides[i].data());
+			in_buffers[i] = cl::Buffer(context, CL_MEM_USE_HOST_PTR | CL_MEM_READ_ONLY, sizeof(double) * data_sizes_input[i], inputs[i]);
+			in_stride_buffers[i] = cl::Buffer(context, CL_MEM_USE_HOST_PTR | CL_MEM_READ_ONLY, sizeof(int) * dimensions, input_strides[i].data());
+			in_offset_buffers[i] = cl::Buffer(context, CL_MEM_USE_HOST_PTR | CL_MEM_READ_ONLY, sizeof(int) * dimensions, input_offset[i].data());
 		}
 
-		for (int i = 0; i < num_out; i++){
-			out_buffers[i] = cl::Buffer(context, CL_MEM_USE_HOST_PTR | CL_MEM_WRITE_ONLY, sizeof(double) * output_sizes[i], outputs[i]);
-			out_stride_buffers[i] = cl::Buffer(context, CL_MEM_USE_HOST_PTR | CL_MEM_READ_ONLY, sizeof(int) * len_stride, output_strides[i].data()); 
-		}
+		out_buffer = cl::Buffer(context, CL_MEM_USE_HOST_PTR | CL_MEM_WRITE_ONLY, sizeof(double) * data_size_output, outputs[0]);
+		out_shape_buffer = cl::Buffer(context, CL_MEM_USE_HOST_PTR | CL_MEM_WRITE_ONLY, sizeof(int) * dimensions, output_shape.data());
+		out_stride_buffer = cl::Buffer(context, CL_MEM_USE_HOST_PTR | CL_MEM_READ_ONLY, sizeof(int) * dimensions, output_stride.data());
+		out_offset_buffer = cl::Buffer(context, CL_MEM_USE_HOST_PTR | CL_MEM_READ_ONLY, sizeof(int) * dimensions, output_offset.data());
+		out_offset_end_buffer = cl::Buffer(context, CL_MEM_USE_HOST_PTR | CL_MEM_READ_ONLY, sizeof(int) * dimensions, output_offset_end.data());
 
 		std::cout << "INFO: Buffers has been created" << std::endl;
 
 		//set inputs.
-		for (int i = 0; i < num_in; i++)
-		{
+		for (int i = 0; i < num_in; i++){
 			kernel.setArg(i, in_buffers[i]);
 			q.enqueueMigrateMemObjects({in_buffers[i]}, 0);
 		}
 
+		std::cout << "INFO: inputs has been set." <<std::endl;
+
 		//set strides
-		for (int i = 0; i < 2; i++){
-			kernel.setArg(2 + i, in_stride_buffers[i]);
-			q.enqueueMigrateMemObjects({in_stride_buffers[i]}, 0);
+		for (int i = num_in; i < (2 * num_in); i++){
+			kernel.setArg(i, in_stride_buffers[i-num_in]);
+			q.enqueueMigrateMemObjects({in_stride_buffers[i-num_in]}, 0);
 		}
+
+		std::cout << "INFO: strides has been set." <<std::endl;
+
+		//set offsets
+		for (int i = (2 * num_in); i < (3 * num_in); i++){
+			kernel.setArg(i, in_offset_buffers[i-2*num_in]);
+			q.enqueueMigrateMemObjects({in_offset_buffers[i-2*num_in]}, 0);
+		}
+
+		std::cout << "INFO: offests has been set." <<std::endl;
 
 		//set outputs
-		for (int i = 0; i < num_out; i++)
-		{
-			kernel.setArg(i + num_in + 2, out_buffers[i]);
-		}
+		kernel.setArg(3 * num_in, out_buffer);			  //arg6
+		kernel.setArg(3 * num_in + 1, out_shape_buffer);	  //arg7
+		kernel.setArg(3 * num_in + 2, out_stride_buffer); //arg8
+		kernel.setArg(3 * num_in + 3, out_offset_buffer);		  //arg9
+		kernel.setArg(3 * num_in + 4, out_offset_end_buffer);  //arg10
 
-		//set output shape + stride
-		kernel.setArg(5, out_shape_buffer);
-		kernel.setArg(6, out_stride_buffers[0]);
+		q.enqueueMigrateMemObjects({out_shape_buffer}, 0);
+		q.enqueueMigrateMemObjects({out_stride_buffer}, 0);
+		q.enqueueMigrateMemObjects({out_offset_buffer}, 0);
+		q.enqueueMigrateMemObjects({out_offset_end_buffer}, 0);
+
+		std::cout << "INFO: outputs has been set\n";
+
+		q.finish();
 
 		std::cout << "INFO: Arguments are set\n";
 
-		q.enqueueMigrateMemObjects({out_shape_buffer}, 0);
-		q.enqueueMigrateMemObjects({out_stride_buffers[0]}, 0);
-
 		q.enqueueTask(kernel);
 		q.finish();
-		q.enqueueMigrateMemObjects({out_buffers[0]}, CL_MIGRATE_MEM_OBJECT_HOST); // 1 : migrate from dev to host
+		q.enqueueMigrateMemObjects({out_buffer}, CL_MIGRATE_MEM_OBJECT_HOST); // 1 : migrate from dev to host
 
 		std::cout << "INFO: Migrated to back to host" << std::endl;
 
@@ -315,7 +436,7 @@ int main(int argc, const char *argv[])
 	xt::xarray<double> b_tri = xt::zeros<double>({X-2, Y-2, Z});
 	xt::xarray<double> c_tri = xt::zeros<double>({X-2, Y-2, Z});
 	xt::xarray<double> d_tri = xt::zeros<double>({X-2, Y-2, Z});
-	xt::xarray<double> delta = xt::zeros<double>({X-2, Y-2, Z});
+	xt::xarray<double> delta = xt::zeros<double>({X-4, Y-4, Z});
 
 
 	/* this block might be not needed!
@@ -365,56 +486,34 @@ int main(int argc, const char *argv[])
 	std::vector<double *> inputs;
 	std::vector<double *> outputs;
 
-
+	/*
 	inputs = {
 				xt::view(tke, xt::all(), xt::all(), xt::all(), tau).data(), 
 				flux_east.data()
 			}; 
-
 	outputs = {sqrttke.data()};
 	run_kernel("vmax", size_3d, inputs, outputs, devices, context, bins, q);
 
 	inputs = {sqrttke.data()};
 	outputs = {sqrttke.data()};
 	run_kernel("vsqrt", size_3d, inputs, outputs, devices, context, bins, q);
+	*/
 
-	xt::xarray<double> ones = xt::ones<double>({X-2, Y-2, Z});
-
-	xt::xarray<double> cs = xt::sum(delta);
+	xt::xarray<double> one = xt::ones<double>({1});
+	xt::xarray<double> test = xt::arange(9).reshape({3,3});
+	xt::xarray<double> res = xt::zeros<double>({4, 4});
 
 	inputs = {	
-				ones.data(),
-				xt::view(dzt, xt::newaxis(), xt::newaxis(), xt::range(1, _)).data()
+				one.data(),
+				dzt.data(),
 			};
-	outputs = { xt::view(delta, xt::all(), xt::all(), xt::range(_, -1)).data() };
-	run_kernel("vdiv", (X-2)*(Y-2)*Z, inputs, outputs, devices, context, bins, q);
-
-	std::cout << dzt << std::endl;
-
-	inputs = { 
-				xt::view(delta, xt::all(), xt::all(), xt::range(_, -1)).data(), 
-				xt::eval(ones * 0.5 * alpha_tke * dt_mom).data() // we need eval as xtensor is lazy!
-			};
-	outputs = { xt::view(delta, xt::all(), xt::all(), xt::range(_, -1)).data() };
-	run_kernel("vmult", (X-2)*(Y-2)*Z, inputs, outputs, devices, context, bins, q); //this is the most useless op. we multiple by a scalar, but we allready do the multiplication on the host anyways.. (ew)
-
-	inputs = {
-				xt::view(delta, xt::all(), xt::all(), xt::range(_, -1)).data(), 
-				xt::view(kappaM, xt::range(2, -2), xt::range(2, -2), xt::range(_,-1)).data()
-			};
-	outputs = { xt::view(delta, xt::all(), xt::all(), xt::range(_, -1)).data() };
-	run_kernel("vmult", (X-2)*(Y-2)*Z, inputs, outputs, devices, context, bins, q);
-
-	inputs = {
-				xt::view(delta, xt::all(), xt::all(), xt::range(_, -1)).data(), 
-				xt::view(kappaM, xt::range(2, -2), xt::range(2, -2), xt::range(1,_)).data()
-			};
-	outputs = { xt::view(delta, xt::all(), xt::all(), xt::range(_, -1)).data() };
-	run_kernel("vadd", (X-2)*(Y-2)*Z, inputs, outputs, devices, context, bins, q);
-
-	cs = xt::sum(delta);
-
-	std::cout << cs << std::endl;
+	outputs = { delta.data() };
+	run_broadcast_kernel("div3d", inputs, outputs, 
+		{1}, {Z}, {X-4, Y-4, Z},		//shapes
+		{0}, {1,}, {0, 0, 0,},			//start index
+		{0}, {0,}, {0, 0, -1}, 		//negativ end index
+		devices, context, bins, q);
+	std::cout << delta;
 
 	return 0;
 }
